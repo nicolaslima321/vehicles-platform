f7efb0730ac06a06a06d63c5dc26d001b9419e45
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-06-13 11:22:05 -0300

    Trying to fix SCSS variables missing in production

--- layouts/Header/Header.jsx
+++ layouts/Header/Header.jsx
@@ -19,17 +19,16 @@ import {
   Menu as MenuIcon,
   ChevronLeft as ChevronLeftIcon,
   ChevronRight as ChevronRightIcon,
-  MoveToInbox as InboxIcon,
-  Mail as MailIcon,
 } from '@mui/icons-material';
 
-import styles from '/styles/layouts/Header.module.scss';
-import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const drawerWidth = 240;
 
 export default function Header({ children }) {
   const theme = useTheme();
+  const router = useRouter();
+
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -42,7 +41,7 @@ export default function Header({ children }) {
 
   return (
     <>
-      <AppBar className={styles.headerBar} open={open}>
+      <AppBar sx={{ backgroundColor: '#ef404b' }} open={open}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -77,24 +76,11 @@ export default function Header({ children }) {
         </>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Initial Page'].map((text) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
+              <ListItemButton onClick={()=> router.push('/')}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <ChevronRightIcon />
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>

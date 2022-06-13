6e1496c33434527cbf7d9665dcab5935a9c39aaf
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-06-13 10:26:34 -0300

    Trying to fix SCSS variables missing in production

--- next.config.js
+++ next.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const customProdConfigs =
+  isProduction
+    ? {}
+    : {
+        sassOptions: {
+          includePaths: [path.join(__dirname, 'styles')],
+          prependData: `@import "variables.module.scss";`,
+        },
+      };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    apiUrl: process.env.NODE_ENV === 'production'
+    apiUrl: isProduction
       ? 'https://shippify-nicolas-challenge.herokuapp.com/api'
       : 'http://localhost:8080/api',
   },
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "variables.module.scss";`,
-  },
+  ...customProdConfigs,
 };
 
 module.exports = nextConfig
